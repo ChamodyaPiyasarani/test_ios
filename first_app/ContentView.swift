febf6d561e00791dfff34dd667aca854f5ed1d3e
ChamodyaPiyasarani <chamo.piyasarani2002@gmail.com>
2026-01-10 13:37:06 +0530

    Initial Commit

---
+++ first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first_app
+//
+//  Created by cobsccomp242p-030 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first_app
+//
+//  Created by cobsccomp242p-030 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
